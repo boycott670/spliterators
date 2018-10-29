c34595404128ac27e9ca68a6c69409864d3b57d7
Ibrahim <diihnini@gmail.com>
2018-10-29 22:18:33 +0100

    enumeratespliterator

--- src/main/java/spliterators/EnumerateSpliterator.java
+++ src/main/java/spliterators/EnumerateSpliterator.java
@@ -1,43 +1,40 @@
 package spliterators;
 
+import java.util.Comparator;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 
-public final class EnumerateSpliterator<T> implements Spliterator<EnumeratePair<T>>
-{
+public class EnumerateSpliterator<T> implements Spliterator<EnumeratePair<T>> {
+
     private final Spliterator<T> source;
 
-    public EnumerateSpliterator(Spliterator<T> source)
-    {
+    public EnumerateSpliterator(Spliterator<T> source) {
         this.source = source;
     }
 
     @Override
-    public boolean tryAdvance(Consumer<? super EnumeratePair<T>> consumer)
-    {
+    public boolean tryAdvance(Consumer<? super EnumeratePair<T>> action) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Spliterator<EnumeratePair<T>> trySplit()
-    {
-        final Spliterator<T> subSpliterator = source.trySplit();
-
-        if (subSpliterator != null)
-            return new EnumerateSpliterator<>(subSpliterator);
-
-        return null;
+    public Spliterator<EnumeratePair<T>> trySplit() {
+        Spliterator<T> splitSource = source.trySplit();
+        return splitSource == null ? null : new EnumerateSpliterator<>(splitSource);
     }
 
     @Override
-    public long estimateSize()
-    {
+    public long estimateSize() {
         return source.estimateSize();
     }
 
     @Override
-    public int characteristics()
-    {
-        return source.characteristics() | DISTINCT | NONNULL | SORTED;
+    public int characteristics() {
+        return source.characteristics() | SORTED | NONNULL | DISTINCT | ORDERED;
+    }
+
+    @Override
+    public Comparator<? super EnumeratePair<T>> getComparator() {
+        return null;
     }
 }
